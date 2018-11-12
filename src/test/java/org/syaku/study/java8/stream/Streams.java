f4eb9c72ec8fd2e1fd9f5a2adc0f8a1f9f82f838
syaku <syaku@naver.com>
2018-11-12 21:49:59 +0900

    java8 stream 작성중.

--- src/test/java/org/syaku/study/java8/stream/Streams.java
+++ src/test/java/org/syaku/study/java8/stream/Streams.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 import lombok.extern.log4j.Log4j2;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Seok Kyun. Choi. 최석균 (Syaku)
  * @since 12/11/2018
@@ -18,37 +20,64 @@ import lombok.extern.log4j.Log4j2;
 public class Streams {
   @Test
   public void create() {
-    Arrays.stream(new String[]{ "가", "나", "다" });
-    Arrays.asList("가", "나", "다").stream();
-    Stream.of("가", "나", "다");
-    Stream.<String>builder().add("가").add("나").add("다").build();
-    // 가가가가가가가가가가
-    Stream.generate(() -> "가").limit(10);
-    // 가가가가가가가가가가
-    Stream.iterate("가", v -> v).limit(10);
 
-    // IntStream, LongStream, DoubleStream
-    IntStream.range(1, 5);
-    IntStream.rangeClosed(1, 5);
+    Stream.of("가", "나", "다").forEach(log::debug);
+
+    Stream.<String>builder().add("가").add("나").add("다").build().forEach(log::debug);
+
+    Stream.generate(() -> "가").limit(10).forEach(log::debug);
+
+    Stream.iterate("가", v -> v).limit(10).forEach(log::debug);
+
+    Stream.concat(Stream.of(1, 2, 3), Stream.of(4, 5))
+            .forEach(log::debug); // console.log
+
+    Stream.empty();
+
+    // IntStream, LongStream, DoubleStream 생략한다.
+    IntStream.range(1, 5).forEach(log::debug);
+    IntStream.rangeClosed(1, 5).forEach(log::debug);
   }
 
   private List<String> data = Arrays.asList("Green", "Red", "Black", "Yellow");
 
   @Test
   public void filter() {
-    List<String> result = data.stream().filter("Green"::equals).collect(Collectors.toList());
-    result.forEach(n -> log.debug("{}", n));
+    data.stream().filter("Green"::equals).collect(Collectors.toList()).forEach(log::debug);
   }
 
   @Test
   public void map() {
-    List<Integer> result = data.stream().map(n -> n.length()).collect(Collectors.toList());
-    result.forEach(n -> log.debug("{}", n));
+    data.stream().map(String::length).collect(Collectors.toList()).forEach(log::debug);
   }
 
   @Test
   public void flatMap() {
-    List<List<String>> list = Arrays.asList(Arrays.asList("a"), Arrays.asList("b"));
-    List<String> list2 = list.stream().flatMap(Collection::stream).peek(log::debug).collect(Collectors.toList());
+    List<List<String>> list = Arrays.asList(Arrays.asList("a", "aa"), Arrays.asList("b", "bb"));
+    list.stream().flatMap(Collection::stream).forEach(log::debug);
+  }
+
+  @Test
+  public void forEach() {
+    Random random = new Random();
+    random.ints().limit(10).forEach(log::debug);
+  }
+
+  @Test
+  public void sorted() {
+    IntStream.of(15,3,234,32,432,4,454,35,435).sorted().forEach(log::debug);
+    // boxed 동작으로 Integer 로 박싱할 수 있다.
+    IntStream.of(15,3,234,32,432,4,454,35,435).sorted().boxed().forEach(log::debug);
+    // 내림차순 정렬 제너릭 유형을 사용하므로 원시형을 사용할 수 없어 박싱한다.
+    IntStream.of(1,2,3,4,5).boxed().sorted(Comparator.reverseOrder()).forEach(log::debug);
+  }
+
+  @Test
+  public void calculating() {
+    assertEquals(IntStream.of(1,2,3,4).sum(), 10);
+    assertEquals(IntStream.of(1,2,3,4).count(), 4);
+    assertEquals(IntStream.of(1,2,3,4).max().getAsInt(), 4);
+    assertEquals(IntStream.of(1,2,3,4).min().getAsInt(), 1);
+    assertEquals(IntStream.of(1,2,3,4).average().getAsDouble(), 2.5, 2.5);
   }
 }
